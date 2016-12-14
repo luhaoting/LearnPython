12113ec5422cd259b0b2248a556a0aaba4152061
luhaoting <luhaoting1990@163.com>
2016-12-14 02:20:50 -0800

    快速排序

---
+++ Algorithm/QuickSort.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import random
+
+srcList = [random.randrange(0, 100, 1) for x in range(0, 10)]
+
+def QuickSort(list , left , right):
+    pivot = list[left] # 起始点作为参考点 
+    #pivot = list[(right - left) // 2 ] #也可以选取中间点
+    subleft = left
+    subright = right
+    while list[subleft] < pivot:
+        subleft += 1
+    while list[subright] > pivot:
+        subright -= 1
+    if subleft <= subright: # == 的时候需要 偏移下标
+        tmp = list[subright]
+        list[subright] = list[subleft]
+        subright -= 1
+        list[subleft] = tmp
+        subleft += 1
+    if left < subright: # 递归左区间
+        QuickSort(list, left, subright)
+    if subleft < right:
+        QuickSort(list, subleft, right)
+    return list
+
+
+print("Src : ", srcList)
+QuickSort(srcList, 0, len(srcList) - 1)
+print("Sort Result ", srcList)
---
+++ Other/other.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+x = 10
+def space():
+    print(x)
+    #x = 100 #加了这句编译错误 Python的作用域判断真是奇葩 没有同名局部屏蔽全局变量的设定吗？
+space()
+
+print(100.11123//2)
+
