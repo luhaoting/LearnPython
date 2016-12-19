2a190b357cf77c09b586c71f414020631f65e7b9
luhaoting <luhaoting1990@163.com>
2016-12-18 18:51:12 -0800

    修正快速算法错误

--- Algorithm/QuickSort.py
+++ Algorithm/QuickSort.py
@@ -2,23 +2,25 @@
 # -*- coding: utf-8 -*-
 import random
 
-srcList = [random.randrange(0, 100, 1) for x in range(0, 10)]
+#srcList = [random.randrange(0, 100, 1) for x in range(0, 10)]
 
+srcList = [4,6,7,9,5,3,1,2]
 def QuickSort(list, left, right):
-    pivot = list[left] # 起始点作为参考点 
-    #pivot = list[(right - left) // 2 ] #也可以选取中间点
+    #pivot = list[left] # 起始点作为参考点 
+    pivot = list[int((right + left) // 2) ] #也可以选取中间点
     subleft = left
     subright = right
-    while list[subleft] < pivot:
-        subleft += 1
-    while list[subright] > pivot:
-        subright -= 1
-    if subleft <= subright: # == 的时候需要 偏移下标 .. []...中间的值已经在正确的位置了不需要加入分区递归
-        tmp = list[subright]
-        list[subright] = list[subleft]
-        subright -= 1
-        list[subleft] = tmp
-        subleft += 1
+    while subleft <= subright:
+        while list[subleft] < pivot:
+            subleft += 1
+        while pivot < list[subright]:
+            subright -= 1
+        if subleft <= subright: # == 的时候需要 偏移下标 .. []...中间的值已经在正确的位置了不需要加入分区递归
+            tmp = list[subright]
+            list[subright] = list[subleft]
+            subright -= 1
+            list[subleft] = tmp
+            subleft += 1
     if left < subright: # 递归左区间
         QuickSort(list, left, subright)
     if subleft < right:
