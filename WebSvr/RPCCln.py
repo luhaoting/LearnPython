5c89fa8cd0a0576edb06c5dd4f7c447d10ecb97b
luhaoting <luhaoting1990@163.com>
2017-01-12 17:47:00 +0800

    rpc

--- WebSvr/RPCCln.py
+++ WebSvr/RPCCln.py
@@ -1,8 +1,5 @@
 from xmlrpc.client import ServerProxy
 
 proxy = ServerProxy('http://localhost:8000/')
-#a = proxy.file_reader('/tmp/secret.txt')
-
-# 下面是windows的路径写法
 a = proxy.file_reader('./WebSnapshot.py')
 print(a) # 打印出文件内容
